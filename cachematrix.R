3421b47b2e49fd0122d6814d565d379578ffbb32
Xinxin Wang <xinxin@Xinxins-MacBook-Pro.local>
2024-08-11 10:56:17 -0400

    completed R code for hw3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_m <- NULL
+    set <- function(y) {
+        x <<- y
+        inv_m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv_m <<- inverse
+    getinv <- function() inv_m
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes and returns the inverse of the special "matrix returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if (identical(x$get, x$set)) {
+        inv_m <- x$getinv()
+        if (!is.NULL(inv_m)) {
+            return(inv_m)
+        }
+    }
+    else {
+    data <- x$get()
+    inv_m <- solve(data, ...)
+    x$setinv(inv_m)
+    return (inv_m)
+    }
 }
+
